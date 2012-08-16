652aaf9b4403e6d548f594961b58082ff920dc2c
Iliya Grushevskiy <iliya.gr@gmail.com>
2012-08-16 19:10:36 +0400

    Add log.

--- log.c
+++ log.c
@@ -5,5 +5,37 @@
 //  Created by Iliya Grushveskiy on 16.08.12.
 //  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
 //
-
+#include "log.h"
 #include <stdio.h>
+
+static char* sg_log_app   = NULL;
+static int   sg_log_level = SG_LOG_INFO;
+
+void sg_vlog(int level, const char* fmt, va_list vl) {
+    if (level <= sg_log_level) {
+        
+        if(sg_log_app) {
+            fprintf(stderr, "%s: ", sg_log_app);
+        }
+        
+        vfprintf(stderr, fmt, vl);
+        fprintf(stderr, "\n");
+    }
+}
+
+void sg_log(int level, const char* fmt, ...) {
+    va_list vl;
+    va_start(vl, fmt);
+    sg_vlog(level, fmt, vl);
+    va_end(vl);
+}
+
+void sg_log_init(char* app) {
+    sg_log_app   = app;
+    av_log_set_level(sg_log_level);
+}
+
+void sg_log_set_level(int level) {
+    sg_log_level = level;
+    av_log_set_level(level);
+}
--- log.h
+++ log.h
@@ -5,10 +5,23 @@
 //  Created by Iliya Grushveskiy on 16.08.12.
 //  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
 //
+#include <libavutil/avutil.h>
 
-#ifndef mediasegmenter_log_h
-#define mediasegmenter_log_h
+#ifndef __SG_LOG__
+#define __SG_LOG__
 
+#define SG_LOG_QUITE    AV_LOG_QUITE
+#define SG_LOG_FATAL    AV_LOG_FATAL
+#define SG_LOG_ERROR    AV_LOG_ERROR
+#define SG_LOG_WARNING  AV_LOG_WARNING
+#define SG_LOG_INFO     AV_LOG_INFO
+#define SG_LOG_VERBOSE  AV_LOG_VERBOSE
+#define SG_LOG_DEBUG    AV_LOG_DEBUG
+
+void sg_log(int level, const char* fmt, ...);
+void sg_vlog(int level, const char* fmt, va_list vl);
+void sg_log_init(char*);
+void sg_log_set_level(int);
 
 
 #endif
